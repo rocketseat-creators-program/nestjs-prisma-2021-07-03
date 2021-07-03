9e2cd3e1eb94ee143f1775bb1c358af430515c45
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-03 15:32:04 -0300

    chore: create prisma and reformat all codebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [UsersModule, PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "@prisma/client";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
